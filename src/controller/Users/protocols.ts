435703c9b6d7d5122ab7b6d4c1c1f0ad6400913d
valderez <valderezpe@gmail.com>
2022-12-31 10:32:15 -0300

    refactore, getController

--- src/controller/Users/protocols.ts
+++ src/controller/Users/protocols.ts
@@ -1,3 +1,10 @@
+import { User } from "../../Models/User";
+import { HttpResponse } from "../protocols";
+
  export interface IUsersController {
-    handle(): any;
+    handle(): Promise <HttpResponse<User []>>;
+}
+
+export interface IUsersRepository{
+    getUsers(): Promise<User[]>
 }
--- src/controller/Users/users-controller.ts
+++ src/controller/Users/users-controller.ts
@@ -1,5 +1,23 @@
-import { IUsersController } from "./protocols";
+import { IUsersController, IUsersRepository } from "./protocols";
 
 export class UsersController implements IUsersController {
-    handle(){}
+constructor( private readonly UsersRepository: IUsersRepository){ }
+     async handle(){
+
+       try {
+        const users = await this.UsersRepository.getUsers();
+        
+        return{
+            statusCode: 200,
+            body:users
+        };
+       } catch (error) {
+        return {
+            statusCode: 500, 
+            body: "sorry not possivel acess"
+        };
+        
+       }
+    
+    }
 }
