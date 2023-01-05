3fe92fdcf033e8f87846c19aa20c0e3338c7c82b
valderez <valderezpe@gmail.com>
2023-01-05 10:58:22 -0300

    feat: DeleteUserContrtoller finalizado.

--- src/controllers/delete-user/protocols.ts
+++ src/controllers/delete-user/protocols.ts
@@ -1,5 +1,10 @@
 import { User } from "../../Models/User";
+import { HttpRequest, HttpResponse } from "../protocols";
 
+export interface IDeleteUserController{
+    handle(httpResponse: HttpRequest<any>): Promise<HttpResponse<User>> 
+}
 export interface IDeleteUserRepository {
+    [x: string]: any;
     delete(id: string): Promise<User>;
 }
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,10 @@ import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-user
 import { MongoClient } from "./database/mongo";
 import { MongoCreateUserRepository } from "./repositories/get-users/create-user/mongo-user";
 import { CreateUserController } from "./controllers/create-user/create-user";
-import { MongoUpdateUserRepository } from "./repositories/get-users/updade-user/mongo-update-user";
 import { UpdateUserController } from "./controllers/update-user/update-user";
+import { MongoDeleteUserRepositoty } from "./repositories/get-users/delete-user/mongo-delete.user";
+import { DeleteUserController } from "./controllers/delete-user/delete.user";
+import { MongoUpdateUserRepository } from "./repositories/get-users/updade-user/mongo-update-user";
 
 
 
@@ -39,12 +41,26 @@ const main = async() =>{
         });
         res.status(statusCode as any).send(body);
     });
+
     app.patch("/client/:id", async (req, res) =>{
         const mongoUpdateUserRepository = new MongoUpdateUserRepository()
         const updateUserController = new UpdateUserController(mongoUpdateUserRepository)
 
         const {body,statusCode} = await updateUserController.handle({
             body: req.body,
+            params:req.params,
+        });
+
+        res.status(statusCode as any).send(body);
+    });
+
+    
+    app.delete("/client/:id", async (req, res) =>{
+        const mongoDeleteUserRepository = new MongoDeleteUserRepositoty();
+
+        const deleteUserController = new DeleteUserController (mongoDeleteUserRepository)
+
+        const {body,statusCode} = await deleteUserController.handle({
             params:req.params,
         });
 
